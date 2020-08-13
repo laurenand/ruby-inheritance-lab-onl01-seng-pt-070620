6a00b84a0e281bdf667f1fd7c8b546e2a04f3094
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-13 00:20:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User 
+  attr_accessor :first_name, :last_name
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end
+  
+  def learn 
+    
+  end
+  
+  def knowledge
+    @knowledge
+  end
 
 end
